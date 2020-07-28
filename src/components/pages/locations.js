08e58d0001c89e7836ab61b0e2a5822a6fcd5dfb
its-nedum <de.magnificent1@gmail.com>
2020-07-28 21:03:45 +0100

    chores(started locations design)

--- src/components/pages/locations.js
+++ src/components/pages/locations.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import '../../styles/locations.css'
 const Locations = () => {
     return (
         <div>
-            <h1>Locations page </h1>
+            <div className="card locations-card">
+                <div className="card-body">
+                    <h3>A journey of a thousand miles begins with a single step ~ Lao Tzu</h3>
+                </div>
+            </div>
         </div>
     )
 }
