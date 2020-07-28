a0c989d8c71a29a8ef19587d8b11e6e62e7633ef
its-nedum <de.magnificent1@gmail.com>
2020-07-28 21:58:12 +0100

    chores(started the contest page)

--- src/components/pages/contest.js
+++ src/components/pages/contest.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../../styles/contest.css'
 
 const Contest = () => {
     return (
         <div>
-            <h1>Contest page</h1>
+            <div className="card contest-card">
+                <div className="card-body">
+                    <h3>It is the contest that delight us, and not the victory ~ Blaise Pascal</h3>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4"></div>
+                    <div className="col-sm-12 col-md-4">
+                        <form>
+                            
+                        </form>
+                    </div>
+                    <div className="col-md-4"></div>
+                </div>
+            </div>
         </div>
     )
 }
