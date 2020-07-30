14bbc3192e326e916a59bd8756acf9cfd2df12d5
its-nedum <de.magnificent1@gmail.com>
2020-07-30 15:01:32 +0100

    features(added months to count down)

--- src/components/helpers/timer.js
+++ src/components/helpers/timer.js
@@ -17,11 +17,11 @@ const Timer = () => {
             const now = new Date().getTime();
             const distance = countdownDate - now;
 
-            const months = '00';
             const days = Math.floor(distance / (1000 * 60 * 60 * 24));
             const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
             const minutes = Math.floor((distance % (1000 * 60 * 60)) /(1000 * 60));
             const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            const months = Math.floor(days / 30.458);
 
             if (distance < 0){
                 // stop our timer
