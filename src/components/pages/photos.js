f97a134566adfdf6e3eb1ab3abf9a844a7f96c1e
its-nedum <de.magnificent1@gmail.com>
2020-07-30 20:22:50 +0100

    features(adde multiple tags)

--- src/components/pages/photos.js
+++ src/components/pages/photos.js
@@ -6,12 +6,11 @@ import '../../styles/photos.css'
 
 const Photos = () => {
     const [photos, setPhotos] = useState([]);
-    let tag = 'bikerace'
     let api_key = '36d171a5b0d688aa172352df116dd6c0'
     useEffect(() => {
         axios({
             method: 'get',
-            url: `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${tag}&format=json&nojsoncallback=1`,
+            url: `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=bikerace,BoulderBikeTour&format=json&nojsoncallback=1`,
         }).then((response) => {
             const { page, perpage, pages, total, photo } = response.data.photos;
             setPhotos(photo);
@@ -20,7 +19,6 @@ const Photos = () => {
         });
     }, []);
 
-    // console.log(photos)
     return (
         <div>
             <div className="card photo-card">
