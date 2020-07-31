800df7526241e97e467fb19aef27a20c0a6d16a6
its-nedum <de.magnificent1@gmail.com>
2020-07-31 14:25:32 +0100

    chores(created map component)

---
+++ src/components/helpers/map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Map = () => {
+    return (
+        <div>
+            <div>
+                <p>Location Map</p>
+            </div>
+        </div>
+    )
+}
+
+export default Map
--- src/components/pages/locations.js
+++ src/components/pages/locations.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../../styles/locations.css'
+import Maps from '../helpers/map'
 const Locations = () => {
     return (
         <div>
             <div className="card locations-card">
                 <div className="card-body">
                     <h3>A journey of a thousand miles begins with a single step ~ Lao Tzu</h3>
+                </div>
+            </div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <Maps />
+                    </div>
                 </div>
             </div>
         </div>
