46a84687920fc4f1fcfe0e6ff75f4aab01a11b6b
its-nedum <de.magnificent1@gmail.com>
2020-08-01 15:47:09 +0100

    chores(modified the map)

--- src/components/helpers/map.js
+++ src/components/helpers/map.js
@@ -5,47 +5,45 @@ import riderImage from '../../images/riders.png'
 
 class Maps extends React.Component {
     state = {
-        riderOne: {
+        rider1: {
             lat: 40.00,
             lng: -105.35,
         },
-        riderTwo: {
+        rider2: {
+            lat: 39.95,
+            lng: -105.24,
+        },
+        rider3: {
             lat: 39.95,
             lng: -105.24,
         },
         zoom: 13,
     }
     
-    riderOne = L.icon({
+    riderIcon = L.icon({
         iconUrl: riderImage,
         iconSize:[38,95],
         iconAnchor:[22,94],
         popupAnchor:[-3,-76]
     })
 
-    riderTwo = L.icon({
-        iconUrl: riderImage,
-        iconSize:[38,95],
-        iconAnchor:[22,94],
-        popupAnchor:[-3,-76]
-    })
     render(){
-        const positionRiderOne = [this.state.riderOne.lat, this.state.riderOne.lng]
-        const positionRiderTwo = [this.state.riderTwo.lat, this.state.riderTwo.lng]
+        const positionRider1 = [this.state.rider1.lat, this.state.rider1.lng]
+        const positionRider2 = [this.state.rider2.lat, this.state.rider2.lng]
     return (
-        <Map className="map" center={positionRiderOne} zoom={this.state.zoom}>
+        <Map className="map" center={positionRider1} zoom={this.state.zoom}>
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={positionRiderOne} icon={this.riderOne}>
+            <Marker position={positionRider1} icon={this.riderIcon}>
             <Popup>
-                Hickory Hills
+                Dustin Green
             </Popup>
             </Marker>
-            <Marker position={positionRiderTwo} icon={this.riderTwo}>
+            <Marker position={positionRider2} icon={this.riderIcon}>
             <Popup>
-                Huntington Beach
+                Jason Finn
             </Popup> 
             </Marker>
         </Map>
