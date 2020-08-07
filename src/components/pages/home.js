a1245928f1e411825ae4f160dfd1ce5fbec5a4d8
its-nedum <de.magnificent1@gmail.com>
2020-08-07 16:00:23 +0100

    features(added carousel)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,6 +2,9 @@ import React from 'react'
 import '../../styles/home.css'
 import Timer from '../helpers/timer'
 import bike from '../../images/bike.jpg'
+import image1 from '../../images/carousel_image1.jpg'
+import image2 from '../../images/carousel_image2.jpg'
+import image3 from '../../images/carousel_image3.jpg'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
@@ -20,7 +23,46 @@ const Home = () => {
 
             <div className="container-fluid">
                 <div className="row timer-banner">
-                    <div className="col-md-8"></div>
+                    <div className="col-sm-12 col-md-8 show-carousel">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                        <ol className="carousel-indicators">
+                            <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                        </ol>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                            <img src={image1} className="d-block w-100 img-fluid" alt="bike"/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </div>
+                            </div>
+                            <div className="carousel-item">
+                            <img src={image2} className="d-block w-100 img-fluid" alt="bike"/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                            </div>
+                            <div className="carousel-item">
+                            <img src={bike} className="d-block w-100 img-fluid" alt="bike"/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
+                        </div>
+                    </div>
                     <div className="col-sm-12 col-md-4 count-down">
                         <div className="row start-date">
                             <div className="text-center">
