263413bba955734412f0bfec6df75546cfe530bf
its-nedum <de.magnificent1@gmail.com>
2020-08-07 15:30:31 +0100

    features(design changes)

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -5,14 +5,13 @@ import '../../styles/navbar.css'
 const Navbar = () => {
     return (
         <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-            <Link className="navbar-brand" to="/">Boulder Bike Tour</Link>
+        <nav className="navbar navbar-expand-lg navbar-light">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
+            <div className="collapse navbar-collapse text-center justify-content-center" id="navbarSupportedContent">
+                <ul className="navbar-nav">
                 <li className="nav-item active">
                     <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                 </li>
