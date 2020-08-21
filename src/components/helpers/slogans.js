edefcc3da4ea501ed6150c3790882b2a3df61ddc
its-nedum <de.magnificent1@gmail.com>
2020-08-21 13:03:05 +0100

    chores(added slogan table)

--- src/components/helpers/slogans.js
+++ src/components/helpers/slogans.js
@@ -3,7 +3,39 @@ import React from 'react'
 const Slogans = () => {
     return (
         <div>
-            
+            <div className="card contest-card">
+                <div className="card-body">
+                    <h3>Here is a list of submitted slogan for the contest</h3>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row mt-4">
+                    
+                    <div className="col-sm-12">
+                    <table className="table table-color table-sm table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Slogan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Dustin</td>
+                                <td>Green</td>
+                                <td>Hickory Hills</td>
+                                <td>Hickory Hills</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
