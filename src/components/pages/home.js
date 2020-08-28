57b6c70f4b74dd972cfda1e466f137b7885aa7ad
its-nedum <de.magnificent1@gmail.com>
2020-08-28 14:05:28 +0100

    chores(added bg image)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import '../../styles/home.css'
 import Timer from '../helpers/timer'
-import bike1 from '../../images/bike1.jpg'
-import bike2 from '../../images/bike2.jpg'
-import bike3 from '../../images/bike3.jpg'
-import bike4 from '../../images/bike4.jpg'
-import bike5 from '../../images/bike5.jpg'
-import bike6 from '../../images/bike6.jpg'
-import bike7 from '../../images/bike7.jpg'
-import bike8 from '../../images/bike8.jpg'
-import image1 from '../../images/carousel_image.jpg'
-import image2 from '../../images/carousel_image2.jpg'
-import image3 from '../../images/carousel_image3.jpg'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
@@ -28,8 +17,8 @@ const Home = () => {
                 </div>   
             </div>
 
-            <div className="container-fluid bg-home">
-                dtfyguhijokpltyu
+            <div className=" bg-home">
+                
             </div>  
         </div>
     )
